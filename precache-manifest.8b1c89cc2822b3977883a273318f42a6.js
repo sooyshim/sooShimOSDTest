ac440bea4c305dbe57d186e43682c79ed69c202f
sooyshim <sooyshim@gmail.com>
2019-07-27 22:53:33 -0400

    Updates

---
+++ precache-manifest.8b1c89cc2822b3977883a273318f42a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a332713b84624a783110077ec8bd9a",
+    "url": "/sooShimOSDTest/index.html"
+  },
+  {
+    "revision": "3c3cfd1dcfe30cfd3b96",
+    "url": "/sooShimOSDTest/static/css/main.70fcb116.chunk.css"
+  },
+  {
+    "revision": "78c596cf5e8d6b5ab950",
+    "url": "/sooShimOSDTest/static/js/2.18711b5e.chunk.js"
+  },
+  {
+    "revision": "3c3cfd1dcfe30cfd3b96",
+    "url": "/sooShimOSDTest/static/js/main.f40b40d8.chunk.js"
+  },
+  {
+    "revision": "4235f110faf13d205a88",
+    "url": "/sooShimOSDTest/static/js/runtime~main.d63cfa61.js"
+  }
+]);
